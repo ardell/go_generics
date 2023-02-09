d0dfdb41b5f0d06b3cadb3d441a4e0e9f6be7443
Jason Ardell <ardell@gmail.com>
2023-02-09 16:46:03 -0700

    Switch PlayingCardDeck to use a generic type instead of interface{}.

--- deck.go
+++ deck.go
@@ -5,15 +5,15 @@ import (
 	"time"
 )
 
-type Deck struct {
-	cards []interface{}
+type Deck[C any] struct {
+	cards []C
 }
 
-func NewPlayingCardDeck() *Deck {
+func NewPlayingCardDeck() *Deck[*PlayingCard] {
 	suits := []string{"Diamonds", "Hearts", "Spades", "Clubs"}
 	ranks := []string{"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"}
 
-	deck := &Deck{}
+	deck := &Deck[*PlayingCard]{}
 	for _, suit := range suits {
 		for _, rank := range ranks {
 			deck.AddCard(NewPlayingCard(suit, rank))
@@ -22,11 +22,11 @@ func NewPlayingCardDeck() *Deck {
 	return deck
 }
 
-func (d *Deck) AddCard(card interface{}) {
+func (d *Deck[C]) AddCard(card C) {
 	d.cards = append(d.cards, card)
 }
 
-func (d *Deck) RandomCard() interface{} {
+func (d *Deck[C]) RandomCard() C {
 	randomNumberGenerator := rand.New(rand.NewSource(time.Now().UnixNano()))
 	cardIndex := randomNumberGenerator.Intn(len(d.cards))
 	return d.cards[cardIndex]
